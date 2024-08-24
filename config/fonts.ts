3691854d22849e2c1213f1da0ad06af2e9988856
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2024-08-24 21:42:02 +0530

    Disable font preloading
    meh

--- config/fonts.ts
+++ config/fonts.ts
@@ -8,22 +8,27 @@ import {
 
 export const bold = DM_Sans({
   weight: ["400", "700", "800", "900"],
+  preload: false,
 });
 
 export const IP = Poppins({
   weight: ["400", "700", "800", "900"],
+  preload: false,
 });
 
 export const code = Source_Code_Pro({
   weight: ["400", "700", "800"],
+  preload: false,
 });
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  preload: false,
 });
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  preload: false,
 });
